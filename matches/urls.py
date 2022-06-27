dacb3705b3b606074440e6e522531e7b020fe3ed
Luciano Castillo <luciano.castillo@moni.com.ar>
2022-06-27 17:05:35 +0300

    Create app matches, configure urls

---
+++ matches/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from matches.views import (
+    ImportLeagueView,
+    PlayerListAPIView,
+    TeamRetrieveAPIView,
+)
+
+urlpatterns = [
+    path('importLeague/<slug:code>/', ImportLeagueView.as_view(), name='import'),
+
+    # API
+    path('players', PlayerListAPIView.as_view()),
+    path('team/<str:name>/', TeamRetrieveAPIView.as_view()),
+]
